aa839950d28aec02ac52d0cb8823203f538a83a5
Antoine 3000 <antoine@beaurin.net>
2023-04-30 16:12:51 +0200

    wip: readVotes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,19 @@ import java.io.File
 fun readVotes (file:File):  List<List<String>> {
 val candidatesList= mutableListOf<List<String>>()
     file.forEachLine {
-        candidatesList.add(it.split(","," ").map { name-> name.trim()
+        candidatesList.add(it.split(",").map { name-> name.trim()
             })
     }
     return candidatesList
-    println(candidatesList)
+
 }
 
 
 fun main() {
 
     val file = File("src/votes/300x6.txt")
-    readVotes(file);
+    val votes= readVotes(file);
+    println(votes)
+
 
 }
