1a86e0a50f70f25e36b4fb948bb83ebbf917df42
Antoine 3000 <antoine@beaurin.net>
2023-04-30 16:43:02 +0200

    feat: firstRound and ignore blankspace in readVotes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,10 @@ import java.io.File
 
 fun readVotes (file:File):  List<List<String>> {
 val candidatesList= mutableListOf<List<String>>()
-    file.forEachLine {
-        candidatesList.add(it.split(",").map { name-> name.trim()
-            })
+    file.forEachLine {line->
+        if (line.isNotBlank()) {
+            candidatesList.add(line.split(","," ").map { name -> name.trim() })
+        }
     }
     return candidatesList
 
@@ -15,11 +16,23 @@ fun candidates(votes: List<List<String>>): List<String> {
     return candidates.sorted()
 }
 
+fun firstRound(candidates: List<String>, votes: List<List<String>>): Map<String, Int> {
+    val results = mutableMapOf<String, Int>()
+    candidates.forEach { candidate ->
+        results[candidate] = 0
+    }
+    votes.forEach { ballot ->
+        results[ballot[0]] = results[ballot[0]]!! + 1
+    }
+    return results
+}
+
 fun main() {
 
     val file = File("src/votes/300x6.txt")
     val votes= readVotes(file);
-    print(candidates(votes))
+    val candidate = candidates(votes)
+    print(firstRound(candidate,votes))
    // println(votes)
 
 
